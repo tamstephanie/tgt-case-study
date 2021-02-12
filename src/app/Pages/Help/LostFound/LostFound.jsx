abd8c891183b61f26440185c6cccf5ba9869c323
Stephanie Tam <tam.stephaniek@gmail.com>
2021-02-12 03:08:58 -0500

    Update Lost & Found page and commit ErrorPage.test since it somehow didn't get committed earlier

--- src/app/Pages/Help/LostFound/LostFound.jsx
+++ src/app/Pages/Help/LostFound/LostFound.jsx
@@ -2,12 +2,13 @@ import React, {PureComponent} from "react";
 import _ from "lodash";
 import {Paper, Typography, withStyles} from "@material-ui/core";
 
-// import MountedComponent from "app/Utilities/MountedComponent";
 import AppPage from "app/General/Containers/AppPage/AppPage";
 import {LostFoundStyle} from "./LostFound.style";
 
 /**
  * The Lost & Found page
+ *
+ * @note The link in this page is 
  */
 class LostFound extends PureComponent {
     constructor(props) {
@@ -28,9 +29,16 @@ class LostFound extends PureComponent {
                     Customer Relations/Lost &amp; Found staff are available by phone from
                     8 A.M. to 4:30 P.M. Monday through Friday. Please contact us at 612-373-3333,
                     option 3 for compliments, complaints, concerns related to our service, facilities,
-                    equipment, website, mobile app, or lost &amp; found or use these online forms.
+                    equipment, website, mobile app, or lost &amp; found or use&nbsp;
+                    <a href="#/contact-us">these online forms</a>.
                 </Typography>
-                <Typography></Typography>
+                <Typography paragraph color="textSecondary">
+                    Customers who have lost an item and are wondering if we have it, need to contact us
+                    via phone or email. Customer Relations/Lost &amp; Found staff will contact customers
+                    if their item was recovered to make an appointment for pick-up or arrange to have
+                    the item mailed. The onsite Lost &amp; Found window is no longer open to the general
+                    public for walk-up inquiries.
+                </Typography>
             </React.Fragment>
         );
     }
